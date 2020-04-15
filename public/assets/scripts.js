0606703b53e9be23b4d909690767f920c8085d02
Jason Poindexter <poindexter.json@gmail.com>
2020-04-15 13:25:23 -0700

    Format

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -7,23 +7,25 @@ const setGeneratedNameEl = (name) => {
     textEl.innerHTML = `Congrats! Your new playa name is: <h1><b>${name}</b></h1>`
 };
 let generatedName = window.localStorage.getItem('generatedName');
-if(generatedName) setGeneratedNameEl(generatedName);
+if (generatedName) setGeneratedNameEl(generatedName);
 submissionsRef.onSnapshot((snapshot) => {
     // Only show all submissions if they have generated a name
-    if(!generatedName) return;
+    if (!generatedName) return;
     // Remove all children
     submissionsEl.innerHTML = '';
     snapshot.docs.forEach(submission => {
         let submissionEl = document.createElement('span');
         submissionEl.classList.add('submission');
         submissionEl.innerText = `${baseName}${submission.data().id} - ${submission.data().text}`;
+        // removes google's Touch to Search
+        // https://stackoverflow.com/questions/30648401/disable-mobile-chrome-43s-touch-to-search-feature-programmatically
         submissionEl.setAttribute('role', 'textbox');
         submissionsEl.appendChild(submissionEl);
     })
 });
 
 const generateName = async () => {
-    if(generatedName) return;
+    if (generatedName) return;
     const inputEl = document.getElementById('desc');
     const inputErrEl = document.getElementById('input-error');
     // Handle error messages if no desc input
@@ -44,7 +46,7 @@ const generateName = async () => {
             window.localStorage.setItem('generatedName', generatedName);
             if (doc.data() && doc.data().id) setGeneratedNameEl(generatedName);
         });
-    } catch(error) {
+    } catch (error) {
         // TODO: show error to user if this fails
         console.error("Error adding document: ", error);
     }
