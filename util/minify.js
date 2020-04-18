3cfbc795fc4e07f42a140d6207a74d54ec7e5fc9
Jason Poindexter <poindexter.json@gmail.com>
2020-04-18 14:13:45 -0700

    Implement `npm run deploy` that minifies before deploying to firebase

---
+++ util/minify.js
@@ -0,0 +1,39 @@
+// Minify all files in the 'public' directory qnd export to 'dist' directory
+// NOTE: file extensions not in 'extensions' will not be copied over
+
+const minify = require('minify');
+const fs = require('fs');
+const { resolve } = require('path');
+const ncp = require('ncp').ncp;
+
+// Valid minify file extensions
+const extensions = ['.html', '.js', '.css'];
+
+// Gets files recursively
+const getFiles = async (dir, extensions) => {
+    const dirents = await fs.readdirSync(dir, { withFileTypes: true });
+    const files = await Promise.all(dirents.map((dirent) => {
+        const res = resolve(dir, dirent.name);
+        return dirent.isDirectory() ? getFiles(res, extensions) : res;
+    }));
+    return Array.prototype.concat(...files).filter(path => extensions.some(extension => path.includes(extension, path.length - extension.length)));
+}
+
+(async () => {
+    const srcPath = `${process.cwd()}/public`;
+    const destPath = `${process.cwd()}/dist`;
+    // Copy all files to move any files that will not be minified
+    ncp(srcPath, destPath, async () => {
+        // Minify and move shit
+        const paths = await getFiles(srcPath, extensions);
+        const ignore = await Promise.all(paths.map(async (path) => {
+            try {
+                const minified = await minify(path);
+                await fs.writeFileSync(path.replace(srcPath, destPath), minified);
+            } catch(err) { console.log(err) }
+        }))
+    })
+
+})();
+
+
